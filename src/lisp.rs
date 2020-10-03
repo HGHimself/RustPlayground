51c277ce774745024fcec9980d50bda53699061c
HG King <hgmaxwellking@gmail.com>
2020-10-02 21:10:42 -0400

    got the lisp tests passing :

--- src/lisp.rs
+++ src/lisp.rs
@@ -1,55 +1,67 @@
 pub mod majin {
 
-    pub type Sexpr = Vec<Elements>;
-
-    pub enum Elements {
-        Op(Op),
-        Nat(u32),
-        Atom(String),
+    #[derive(Debug, PartialEq)]
+    enum Element {
+        Sexpr(Vec<Element>),
+        Scalar(Scalar),
     }
 
-    pub enum Op {
-        Add,
-        Sub,
-        Mul,
-        Div
+    #[derive(Debug, PartialEq)]
+    enum Scalar {
+        Atom(String),
+        Nat(u32),
     }
 
     pub(self) mod parser {
-        use super::Op;
-        use super::Elements;
-        use super::Sexpr;
+        use super::Element;
+        use super::Scalar;
+
         use nom::{
             branch::alt,
-            bytes::complete::{tag},
-            Err,
+            bytes::complete::{is_not, tag, take_while},
+            character::complete::{alphanumeric1, digit1, multispace0, one_of},
+            character::is_digit,
+            combinator::{all_consuming, map, peek, rest},
             error::{Error, ErrorKind},
-            character::complete::{multispace0, alphanumeric1, line_ending},
-            bytes::complete::{take_while},
-            sequence::{preceded, delimited, terminated},
-            multi::{many0},
-            IResult,
+            multi::many0,
+            sequence::{delimited, preceded, terminated},
+            Err, IResult,
         };
 
-        fn parse_operator(i: &str) -> IResult<&str, &str> {
-            alt((
-                tag("+"),
-                tag("-"),
-                tag("*"),
-                tag("/"),
-            ))(i)
-        }
-
-        fn parse_value(i: &str) -> IResult<&str, &str> {
-            preceded(multispace0, alphanumeric1)(i)
-        }
-
-        fn parse_sexpr(i: &str) -> IResult<&str, Vec<&str>> {
-            delimited(
-                tag("("),
-                many0(parse_value),
-                preceded(multispace0, tag(")"))
+        fn parse_nat(i: &str) -> IResult<&str, Scalar> {
+            map(
+                digit1,
+                |c: &str| Scalar::Nat(c.parse::<u32>().unwrap())
             )(i)
+        }
+
+        fn parse_atom(i: &str) -> IResult<&str, Scalar> {
+            map(
+                alphanumeric1,
+                |s: &str| Scalar::Atom(s.to_string())
+            )(i)
+        }
+
+        fn parse_scalar(i: &str) -> IResult<&str, Element> {
+            map(
+                preceded(
+                    multispace0,
+                    alt((parse_nat, parse_atom)),
+                ),
+                |s| Element::Scalar(s)
+            )(i)
+        }
+
+        fn parse_element(i: &str) -> IResult<&str, Element> {
+            alt((parse_scalar, parse_sexpr))(i)
+        }
+
+        fn parse_sexpr(i: &str) -> IResult<&str, Element> {
+            map(delimited(
+                preceded(multispace0, tag("(")),
+                many0(parse_element),
+                preceded(multispace0, tag(")"))
+            ), |v| Element::Sexpr(v) )(i)
         }
 
         #[cfg(test)]
@@ -57,26 +69,112 @@ pub mod majin {
             use super::*;
 
             #[test]
-            fn test_parse_operator() {
-                assert_eq!( parse_operator("*"), Ok(("", "*")));
-                assert_eq!( parse_operator("-"), Ok(("", "-")));
-                assert_eq!( parse_operator("+"), Ok(("", "+")));
-                assert_eq!( parse_operator("/"), Ok(("", "/")));
-                assert_eq!( parse_operator(")"), Err(Err::Error(Error::new(")", ErrorKind::Tag))));
+            fn test_parse_nat() {
+                assert_eq!(parse_nat("12 "), Ok((" ", Scalar::Nat(12))));
+                // assert_eq!(parse_nat("12e"), Ok(("", Err(Err::Error(Error::new("e", ErrorKind::Eof))));
             }
 
             #[test]
-            fn test_parse_value() {
-                assert_eq!( parse_value("applesauce"), Ok(("", "applesauce")));
-                assert_eq!( parse_value(" \napplesauce"), Ok(("", "applesauce")));
-                assert_eq!( parse_value(" \n"), Err(Err::Error(Error::new("", ErrorKind::AlphaNumeric))));
+            fn test_parse_atom() {
+                assert_eq!(
+                    parse_atom("atom"),
+                    Ok(("", Scalar::Atom("atom".to_string())))
+                );
             }
 
             #[test]
-            fn test_parse_sexpr() {
-                assert_eq!( parse_sexpr("()"), Ok(("", vec![])));
-                assert_eq!( parse_sexpr("(applesauce)"), Ok(("", vec!["applesauce"])));
-                assert_eq!( parse_sexpr("(applesauce bananas )"), Ok(("", vec!["applesauce", "bananas"])));
+            fn test_parse_scalar() {
+                assert_eq!(
+                    parse_scalar("12"),
+                    Ok(("", Element::Scalar(Scalar::Nat(12))))
+                );
+
+                assert_eq!(
+                    parse_scalar("atom1 )"),
+                    Ok((" )", Element::Scalar(Scalar::Atom(String::from("atom1")))))
+                );
+
+                assert_eq!(
+                    parse_scalar("atom1 ("),
+                    Ok((" (", Element::Scalar(Scalar::Atom(String::from("atom1")))))
+                );
+
+                assert_eq!(
+                    terminated(parse_scalar, multispace0)("atom1 ("),
+                    Ok(("(", Element::Scalar(Scalar::Atom(String::from("atom1")))))
+                );
+
+                assert_eq!(
+                    preceded(tag("("), parse_scalar)("( atom1 )"),
+                    Ok((" )", Element::Scalar(Scalar::Atom(String::from("atom1")))))
+                );
+            }
+
+            #[test]
+            fn test_parse_element() {
+                assert_eq!(
+                    parse_element("atom1"),
+                    Ok(( "", Element::Scalar(Scalar::Atom(String::from("atom1")))) )
+                );
+
+                assert_eq!(
+                    parse_element("atom1 )"),
+                    Ok(( " )", Element::Scalar(Scalar::Atom(String::from("atom1"))) ))
+                );
+
+                assert_eq!(
+                    parse_element("(atom1)"),
+                    Ok(( "", Element::Sexpr(
+                        vec![Element::Scalar(Scalar::Atom(String::from("atom1")))]
+                    ) ))
+                );
+
+                assert_eq!(
+                    parse_element("( atom1 atom2 )"),
+                    Ok(( "", Element::Sexpr(
+                        vec![
+                            Element::Scalar(Scalar::Atom(String::from("atom1"))),
+                            Element::Scalar(Scalar::Atom(String::from("atom2")))]
+                    ) ))
+                );
+
+                assert_eq!(
+                    parse_element("( atom1 (atom2) )"),
+                    Ok(( "", Element::Sexpr(
+                        vec![
+                            Element::Scalar(Scalar::Atom(String::from("atom1"))),
+                            Element::Sexpr(
+                                vec![
+                                    Element::Scalar(Scalar::Atom(String::from("atom2")))
+                                ]
+                            )
+                        ]
+                    ) ))
+                );
+
+                assert_eq!(
+                    parse_element("( atom1 ((atom2) ( atom3 atom4 )) )"),
+                    Ok(( "", Element::Sexpr(
+                        vec![
+                            Element::Scalar(Scalar::Atom(String::from("atom1"))),
+                            Element::Sexpr(
+                                vec![
+                                    Element::Sexpr(
+                                        vec![
+                                            Element::Scalar(Scalar::Atom(String::from("atom2")))
+                                        ]
+                                    ),
+                                    Element::Sexpr(
+                                        vec![
+                                            Element::Scalar(Scalar::Atom(String::from("atom3"))),
+                                            Element::Scalar(Scalar::Atom(String::from("atom4")))
+                                        ]
+                                    )
+                                ]
+                            )
+                        ],
+                    ) ))
+                );
             }
         }
     }
